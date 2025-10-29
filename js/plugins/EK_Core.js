99889531ea39a95351c3ebb3dc4b126e832e599e
EvaarK <micropcondas@gmail.com>
2025-10-29 07:50:31 -0300

    feat: pequeno ajusteno core

--- js/plugins/EK_Core.js
+++ js/plugins/EK_Core.js
@@ -23,66 +23,79 @@
  * - Lançamento Inicial.
  * ============================================================================
  */
-(function () {
-  try {
-    console.log(`[EK_Core] Iniciando...`);
+(function (global) {
+  "use strict";
+  console.log("%c[EK_Core] %cIniciando...", "color: #17a2b8; font-weight: bold;", "color: inherit;");
 
-    window.Evaark = window.Evaark || {};
-    const Evaark = window.Evaark;
+  global.Evaark = global.Evaark || {};
+  const Evaark = global.Evaark;
 
-    Evaark.Imported = Evaark.Imported || {};
-    Evaark.Imported.Core = true;
+  Evaark.Imported = Evaark.Imported || {};
+  Evaark.Imported.Core = true;
 
-    Evaark.Version = function (major, minor, patch, preRelease, build) {
-      this.major = major || 1;
-      this.minor = minor || 0;
-      this.patch = patch || 0;
+  Evaark.Version = function (major = 1, minor = 0, patch = 0, preRelease, build) {
+    this.major = Number.isInteger(major) ? major : 1;
+    this.minor = Number.isInteger(minor) ? minor : 0;
+    this.patch = Number.isInteger(patch) ? patch : 0;
 
-      if (preRelease) this.preRelease = `-${preRelease}`
-      if (build) this.build = `+${build}`
-    };
+    if (preRelease) this.preRelease = `-${preRelease}`;
+    if (build) this.build = `+${build}`;
+  };
 
-    Evaark.Version.prototype.toString = function () {
-      let version = `v${this.major}.${this.minor}.${this.patch}`;
-      if (this.preRelease) version += this.preRelease;
-      if (this.build) version += this.build;
-      return version;
-    };
+  Evaark.Version.prototype.toString = function () {
+    let v = `v${this.major}.${this.minor}.${this.patch}`;
+    if (this.preRelease) v += this.preRelease;
+    if (this.build) v += this.build;
+    return v;
+  };
 
-    Evaark.version = new Evaark.Version(1, 0, 0, "beta");
+  Evaark.version = new Evaark.Version(1, 0, 0, "beta");
+  Evaark.name = "Core"
 
-    Evaark.debug = function (name, message) {
-      console.debug(`[EK_${name}] DEBUG: ${message}`);
-    };
+  Evaark.debug = function (name, message) {
+    if (console.debug) {
+      console.debug(`%c[EK_${name}] %c${message}`, "color: #6c757d; font-weight: bold;", "color: inherit;");
+    }
+  };
 
-    Evaark.log = function (name, message) {
-      console.log(`[EK_${name}] ${message}`);
-    };
+  Evaark.log = function (name, message) {
+    console.log(`%c[EK_${name}] %c${message}`, "color: #17a2b8; font-weight: bold;", "color: inherit;");
+  };
 
-    Evaark.error = function (name, message) {
-      console.error(`[EK_${name}] ERRO: ${message}`);
-    };
+  Evaark.warn = function (name, message) {
+    console.warn(`%c[EK_${name}] %c${message}`, "color: #ffc107; font-weight: bold;", "color: inherit;");
+  };
 
-    Evaark.createModule = function (name, version) {
-      this[name] = this[name] || {};
-      this[name].version = version || new Evaark.Version(0, 0, 0);
-      this.Imported[name] = true;
+  Evaark.error = function (name, message) {
+    console.error(`%c[EK_${name}] %c${message}`, "color: #dc3545; font-weight: bold;", "color: inherit;");
+  };
+
+  Evaark.createModule = function (name, version) {
+    if (this[name]) {
+      Evaark.warn("Core", `Módulo '${name}' já existe e não será sobrescrito.`);
       return this[name];
-    };
+    }
+    this[name] = {};
+    this[name].version = version || new Evaark.Version(0, 0, 0);
+    this[name].name = name;
+    this.Imported[name] = true;
+    return this[name];
+  };
 
-    Evaark.timesRun = function () {
-      let vezesIniciado = Number(localStorage.getItem("EK-vezesIniciado"));
-      if (!vezesIniciado || isNaN(vezesIniciado)) vezesIniciado = 0;
-      localStorage.setItem("EK-vezesIniciado", ++vezesIniciado);
-    };
+  Evaark.timesRun = function () {
+    try {
+      let vezesIniciado = Number(localStorage.getItem("EK-vezesIniciado")) || 0;
+      localStorage.setItem("EK-vezesIniciado", vezesIniciado + 1);
+    } catch (err) {
+      Evaark.error("Core", "Não foi possível salvar 'vezesIniciado' no localStorage.");
+    }
+  };
 
-    Evaark.main = function () {
-      Evaark.timesRun();
-      Evaark.log("Core", `Carregado com sucesso - ${Evaark.version}`);
-    };
+  Evaark.main = function () {
+    Evaark.timesRun();
 
-    Evaark.main();
-  } catch (err) {
-    console.error(err);
-  }
-})();
+    Evaark.log("Core", `Carregado com sucesso - ${Evaark.version}`);
+  };
+
+  Evaark.main();
+})(window);
